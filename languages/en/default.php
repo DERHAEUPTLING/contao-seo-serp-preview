5ecd895a127e4300748cf478c5371fb8dc83fa75
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2016-02-22 21:19:45 +0100

    Added the first draft of the tests backend module

--- languages/en/default.php
+++ languages/en/default.php
@@ -16,3 +16,10 @@
  */
 $GLOBALS['TL_LANG']['MSC']['seo_serp_preview.headline'] = 'SERP Preview';
 $GLOBALS['TL_LANG']['MSC']['seo_serp_preview.hint']     = 'Start entering the data in your form to enable the SEO SERP preview.';
+
+/**
+ * Back end tests module
+ */
+$GLOBALS['TL_LANG']['MSC']['seo_serp_preview_tests.headline'] = 'SERP Tests';
+$GLOBALS['TL_LANG']['MSC']['seo_serp_preview_tests.empty']    = 'There are no pages to be checked.';
+$GLOBALS['TL_LANG']['MSC']['seo_serp_preview_tests.edit']     = 'Edit page';
