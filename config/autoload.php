f0c26bde26541fd67f605adf4abb22fe09b33127
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2016-07-15 13:07:25 +0200

    Use an external class autoloader

--- config/autoload.php
+++ config/autoload.php
@@ -12,34 +12,9 @@
  */
 
 /**
- * Register the namespace
+ * Register PSR-0 namespace
  */
-ClassLoader::addNamespace('Derhaeuptling\SeoSerpPreview');
-
-/**
- * Register the classes
- */
-ClassLoader::addClasses([
-    // Classes
-    'Derhaeuptling\SeoSerpPreview\PreviewWidget'                   => 'system/modules/seo_serp_preview/src/PreviewWidget.php',
-    'Derhaeuptling\SeoSerpPreview\TestsHandler'                    => 'system/modules/seo_serp_preview/src/TestsHandler.php',
-    'Derhaeuptling\SeoSerpPreview\TestsManager'                    => 'system/modules/seo_serp_preview/src/TestsManager.php',
-
-    // Engines
-    'Derhaeuptling\SeoSerpPreview\Engine\AbstractEngine'           => 'system/modules/seo_serp_preview/src/Engine/AbstractEngine.php',
-    'Derhaeuptling\SeoSerpPreview\Engine\EngineInterface'          => 'system/modules/seo_serp_preview/src/Engine/EngineInterface.php',
-    'Derhaeuptling\SeoSerpPreview\Engine\EventsEngine'             => 'system/modules/seo_serp_preview/src/Engine/EventsEngine.php',
-    'Derhaeuptling\SeoSerpPreview\Engine\NewsEngine'               => 'system/modules/seo_serp_preview/src/Engine/NewsEngine.php',
-    'Derhaeuptling\SeoSerpPreview\Engine\PageEngine'               => 'system/modules/seo_serp_preview/src/Engine/PageEngine.php',
-
-    // Tests
-    'Derhaeuptling\SeoSerpPreview\Test\DescriptionTest'            => 'system/modules/seo_serp_preview/src/Test/DescriptionTest.php',
-    'Derhaeuptling\SeoSerpPreview\Test\TestInterface'              => 'system/modules/seo_serp_preview/src/Test/TestInterface.php',
-
-    // Test exceptions
-    'Derhaeuptling\SeoSerpPreview\Test\Exception\ErrorException'   => 'system/modules/seo_serp_preview/src/Test/Exception/ErrorException.php',
-    'Derhaeuptling\SeoSerpPreview\Test\Exception\WarningException' => 'system/modules/seo_serp_preview/src/Test/Exception/WarningException.php',
-]);
+NamespaceClassLoader::add('Derhaeuptling\SeoSerpPreview', 'system/modules/seo_serp_preview/src');
 
 /**
  * Register the templates
